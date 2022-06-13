274f3f038865492a0f1c13a6f4dbdde9fb7a5f07
Alexander Zharyuk <103115934+AlexanderZharyuk@users.noreply.github.com>
2022-06-13 23:03:26 +0300

    Исправленная версия кода

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import time
+import textwrap
 
 import requests
 import telegram
@@ -6,79 +8,70 @@ import telegram
 from dotenv import load_dotenv
 
 
-def get_user_reviews(devman_token: str) -> dict:
-	url = 'https://dvmn.org/api/user_reviews/'
-	headers = {
-		'Authorization': f'Token {devman_token}',
-	}
+def prepare_message(response: dict) -> str:
+    message = ''
 
-	response = requests.get(url=url, headers=headers)
-	response.raise_for_status()
+    for lesson in response['new_attempts']:
+        lesson_title = lesson['lesson_title']
+        lesson_url = lesson['lesson_url']
 
-	return response.json()
+        if lesson['is_negative']:
+            approve_lesson = 'Преподаватель попросил внести правки в проект.'
+        else:
+            approve_lesson = 'Преподаватель принял работу!'
 
+        message += f"""
+                    <b>Название урока</b>: {lesson_title}
+                    {approve_lesson}
+                    <b>Ссылка на урок</b>: {lesson_url}
+                    """
 
-def create_long_polling(devman_token:str, timestamp: (str, float)) -> dict:
-	url = f'https://dvmn.org/api/long_polling/'
-	headers = {
-		'Authorization': f'Token {devman_token}',
-	}
-
-	params = {
-		'timestamp': timestamp
-	}
-
-	response = requests.get(url=url, headers=headers, params=params)
-	response.raise_for_status()
-
-	return response.json()
+    return textwrap.dedent(message)
 
 
-def get_info_for_message(response: dict) -> str:
-	message = ''
-	for lesson in response['new_attempts']:
-		lesson_title = lesson['lesson_title']
-		lesson_url =  lesson['lesson_url']
-		
-		if lesson['is_negative']:
-			approve_lesson = 'Преподаватель попросил внести правки в проект.'
-		else:
-			approve_lesson = 'Преподаватель принял работу!'
+def send_message(bot: telegram.Bot, chat_id: str, reviews_info: dict) -> None:
+    if len(reviews_info['new_attempts']) > 1:
+        message = 'У вас появились ревью по следующим урокам:'
+    else:
+        message = '🚀 Новое ревью!'
 
-		message += f'<b>Название урока</b>: {lesson_title}\n\n' \
-				   f'{approve_lesson}\n' \
-				   f'<b>Ссылка на урок</b>: {lesson_url}\n\n'
-				   
-	return message
+    message += prepare_message(reviews_info)
+    bot.send_message(text=message, chat_id=chat_id,
+                     parse_mode=telegram.ParseMode.HTML)
 
 
-def send_message(bot: telegram.Bot, chat_id: str, service_response: dict) -> None:
-	if len(service_response['new_attempts']) > 1:
-		message = 'У вас появились ревью по следующим урокам:\n\n'
-	else:
-		message = '🚀 Новое ревью!\n\n'
-		
-	message += get_info_for_message(service_response)
-	bot.send_message(text=message, chat_id=chat_id, parse_mode=telegram.ParseMode.HTML)
+if __name__ == '__main__':
+    load_dotenv()
+    devman_token = os.getenv('DEVMAN_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    bot = telegram.Bot(telegram_token)
+    timestamp = ''
+    url = f'https://dvmn.org/api/long_polling/'
+    headers = {
+        'Authorization': f'Token {devman_token}',
+    }
 
+    while True:
+        params = {
+            'timestamp': timestamp
+        }
+        response_tries = 0
 
+        try:
+            response = requests.get(url=url, headers=headers, params=params)
+            response.raise_for_status()
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            response_tries += 1
+            if response_tries >= 5:
+                time.sleep(60)
+            continue
 
-if __name__=='__main__':
-	load_dotenv()
-	devman_token = os.getenv('DEVMAN_TOKEN')
-	telegram_token = os.getenv('TELEGRAM_TOKEN')
-	telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-	bot = telegram.Bot(telegram_token)
-	timestamp = ''
-
-	while True:
-		try:
-			service_response = create_long_polling(devman_token, timestamp)
-		except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-			continue
-
-		if service_response['status'] == 'timeout':
-			timestamp = service_response['timestamp_to_request']
-		else:
-			timestamp = service_response['last_attempt_timestamp']
-			send_message(bot, telegram_chat_id, service_response)
+        reviews_info = response.json()
+        if reviews_info['status'] == 'timeout':
+            timestamp = reviews_info['timestamp_to_request']
+        else:
+            timestamp = reviews_info['last_attempt_timestamp']
+            send_message(bot, telegram_chat_id, reviews_info)
